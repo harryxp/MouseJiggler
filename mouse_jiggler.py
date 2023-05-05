efb964ff9e9911337451331f4b0fed091812a0c9
Harry Pan <pan.xingzhi@gmail.com>
2023-05-05 10:08:40 -0700

    Move up one pixel instead of down.

--- mouse_jiggler.py
+++ mouse_jiggler.py
@@ -6,5 +6,5 @@ INTERVAL_IN_SECONDS = 60 * 1
 
 while True:
     time.sleep(INTERVAL_IN_SECONDS)
-    pyautogui.move(0, 1)    # move down one pixel
-    pyautogui.move(0, -1)   # cowardly move back
+    pyautogui.move(0, -1)   # move up one pixel
+    pyautogui.move(0, 1)    # cowardly move back
